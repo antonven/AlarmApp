7b96aec3dcae2ea18d008d3418308f94a14238a7
Anton Wycoco <Anton Wycoco>
2016-08-22 23:36:19 +0800

    FifthCommit

--- app/src/main/java/myapps/wycoco/com/alarmapp/StopAlarm.java
+++ app/src/main/java/myapps/wycoco/com/alarmapp/StopAlarm.java
@@ -15,14 +15,17 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class StopAlarm extends AppCompatActivity{
+import java.io.IOException;
 
+public class StopAlarm extends AppCompatActivity{
+    boolean isRunning;
     Button btnContacts;
     TextView tv;
     EditText txtMsg;
@@ -40,8 +43,11 @@ public class StopAlarm extends AppCompatActivity{
         btnContacts = (Button) findViewById(R.id.btnContacts);
         btnStop = (Button)findViewById(R.id.btnStop);
         txtMsg = (EditText)findViewById(R.id.txtMsg);
-//        btnCall = (Button) findViewById(R.id.btnCall);
         tv = (TextView) findViewById(R.id.textView);
+
+
+
+
 
         Intent intent = getIntent();
         try{
@@ -54,18 +60,64 @@ public class StopAlarm extends AppCompatActivity{
         }
         mp.start();
 
+
+        if (!this.isRunning) {
+            this.isRunning = true;
+        }
+
+        else if (this.isRunning) {
+            mp.stop();
+            mp.reset();
+            this.isRunning = false;
+        }
+
+        else if (!this.isRunning) {
+            this.isRunning = false;
+        }
+
+        else if (this.isRunning) {
+            this.isRunning = true;
+        }
+
+        else {
+
+        }
+
+
+
+
+
+
+
+
+
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mp.stop();
-                SmsManager sms = SmsManager.getDefault();
-                sms.sendTextMessage(number, null, txtMsg.getText().toString(), null, null);
-                Toast.makeText(StopAlarm.this, "Alarm Stopped and Message sent!", Toast.LENGTH_LONG).show();
-                Intent gb = new Intent(StopAlarm.this, MainActivity.class);
-                gb.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(gb);
+                try {
+                    if(number != null) {
+                        mp.stop();
+                        SmsManager sms = SmsManager.getDefault();
+                        sms.sendTextMessage(number, null, txtMsg.getText().toString(), null, null);
+                        Toast.makeText(StopAlarm.this, "Alarm Stopped and Message sent!", Toast.LENGTH_LONG).show();
+                        Intent gb = new Intent(StopAlarm.this, MainActivity.class);
+                        gb.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(gb);
+                    }else{
+                        Toast.makeText(StopAlarm.this, "Please Select contact!", Toast.LENGTH_SHORT).show();
+                    }
+                }catch (Exception e){
+                    Toast.makeText(StopAlarm.this, "You have 0 Load balance to send this message!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
+
+
+
+
+
+
 
         btnContacts.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,11 +125,16 @@ public class StopAlarm extends AppCompatActivity{
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 Uri uri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/Contacts/");
                 intent.setDataAndType(uri, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
-//                intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
                 startActivityForResult(intent, REQ_CODE);
             }
         });
     }
+
+
+
+
+
+
 
     @Override
     public void onActivityResult(int reqCode, int resultCode, Intent data) {
@@ -90,13 +147,11 @@ public class StopAlarm extends AppCompatActivity{
                 try{
                     c = getContentResolver().query(uri,new String[]{
                                     ContactsContract.CommonDataKinds.Phone.NUMBER,
-                                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},
-                            null,null,null);
+                                    ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME}, null,null,null);
                     if(c != null && c.moveToFirst()){
                         number = c.getString(0);
                         name = c.getString(1);
                         tv.setText(name + ", " + number);
-
                     }
                 }finally {
                     if(c!=null)
